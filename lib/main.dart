6203c1b6c920f52572836c191e16cbe0301d65a0
Umanga Wijesuriya <127008042+umangawije@users.noreply.github.com>
2025-05-11 19:48:34 +0530

    Grid added

--- lib/main.dart
+++ lib/main.dart
@@ -12,31 +12,24 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.deepPurple[200],
         appBar: AppBar(
-          title: Text("My App Bar", style: TextStyle(color: Colors.white)),
           backgroundColor: Colors.deepPurple,
-          elevation: 0,
-          leading: Icon(Icons.menu, color: Colors.white),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.logout, color: Colors.white),
-            ),
-          ],
-        ),
-        body: Center(
-          child: Container(
-            height: 300,
-            width: 300,
-            decoration: BoxDecoration(
-              color: Colors.deepPurple,
-              //Curve corners
-              borderRadius: BorderRadius.circular(20),
-            ),
-            padding: EdgeInsets.all(25),
-            child: Icon(Icons.favorite, color: Colors.white, size: 100),
+          title: Text(
+            "Horizontal ListView",
+            style: TextStyle(color: Colors.white),
           ),
+          leading: Icon(Icons.menu, color: Colors.white),
+        ),
+        body: GridView.builder(
+          itemCount: 64,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 4,
+          ),
+          itemBuilder:
+              (context, index) => Container(
+                color: Colors.deepPurple,
+                margin: EdgeInsets.all(4),
+              ),
         ),
       ),
     );
