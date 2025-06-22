76b7d72148668cb92119755aacd3ff345ad227db
Umanga Wijesuriya <127008042+umangawije@users.noreply.github.com>
2025-06-23 00:29:19 +0530

    greeeting msg added

--- lib/pages/todo_page.dart
+++ lib/pages/todo_page.dart
@@ -8,8 +8,46 @@ class TodoPage extends StatefulWidget {
 }
 
 class _TodoPageState extends State<TodoPage> {
+  //Text editing controller to manage the text input
+  TextEditingController myController = TextEditingController();
+
+  //greetin message
+  String greetingMessage = "";
+
+  //Method to greet the user with the input text
+  void greetUser() {
+    String userName = myController.text;
+    setState(() {
+      greetingMessage = "Hello " + userName;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: TextField()));
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //Display greeting message
+              Text(greetingMessage),
+
+              TextField(
+                controller: myController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Enter your name",
+                  labelText: "Name",
+                ),
+              ),
+
+              ElevatedButton(onPressed: greetUser, child: Text("Tap")),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
