ce8414eaf6bedfe0b8db425c20c35a65d2b194ee
Umanga Wijesuriya <127008042+umangawije@users.noreply.github.com>
2025-06-22 19:44:39 +0530

    todo page created

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_basics/pages/counter_page.dart';
+import 'package:flutter_basics/pages/todo_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: CounterPage());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: TodoPage());
   }
 }
