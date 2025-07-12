402f21ecb455ded3d1acac292d53a0d86a68ca0b
Umanga Wijesuriya <umanga.w@pram-it.com>
2025-07-12 23:56:38 +0530

    background color change

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,25 +11,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          // 🔹 Gradient background
-          Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Color(0xFF0F2027),
-                  Color(0xFF203A43),
-                  Color(0xFF2C5364),
-                ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
+    return Scaffold(backgroundColor: Colors.yellow[200]);
   }
 }
